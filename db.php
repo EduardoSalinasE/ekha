b1a98dad1c1879afd9623ab5f1fd80e10ecac07d
ekha186 <91392483+ekha186@users.noreply.github.com>
2021-12-01 23:51:04 -0500

    Update db.php

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
     header("Access-Control-Allow-Origin: *");
     $cn = new mysqli("https://databases-auth.000webhost.com/index.php","id17782893_ekha","qdkU>?u\hi64m~OA","id17782893_saintseiya");
-    $rs = $cn->query("SELECT * FROM crearcaballero")
+    $rs = $cn->query("SELECT * FROM crearcaballero");
 ?>
