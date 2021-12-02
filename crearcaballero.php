fe1d8ff87fdd58a23cd19ce817e20f255dc4fc05
ekha186 <91392483+ekha186@users.noreply.github.com>
2021-12-01 22:30:45 -0500

    Add files via upload

---
+++ crearcaballero.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 5.1.1
+ */
+
+/**
+ * Database `saintseiya`
+ */
+
+/* `saintseiya`.`crearcaballero` */
+$crearcaballero = array(
+  array('idPersonaje' => '1','nombrePersonaje' => 'Eduardo','constelacion' => 'Aries','descripcion' => 'Este Caballero tiene fuerza para controlar la mente'),
+  array('idPersonaje' => '2','nombrePersonaje' => 'Kevin','constelacion' => 'Acuario','descripcion' => 'Este Caballero puede hacer helado y es mujeriego'),
+  array('idPersonaje' => '3','nombrePersonaje' => 'Harold','constelacion' => 'Acuario','descripcion' => 'Este caballero esta casado'),
+  array('idPersonaje' => '4','nombrePersonaje' => 'Antony','constelacion' => 'Virgo','descripcion' => 'Este caballero es muy misterioso y come mucho'),
+  array('idPersonaje' => '5','nombrePersonaje' => 'Luis','constelacion' => 'Sagitario','descripcion' => 'Caballero que brilla como el sol mismo, puede cocinar'),
+  array('idPersonaje' => '6','nombrePersonaje' => 'Steven','constelacion' => 'Tauro','descripcion' => 'Este caballero le gusta el free fire')
+);
